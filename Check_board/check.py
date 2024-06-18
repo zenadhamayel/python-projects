d64dbd0af23f7fb08896a7ed4d710a8edf74fd2d
zenadhamayel <zenadhamayel19@gmail.com>
2024-06-18 14:24:34 -0700

    Add check_board Assigement

---
+++ Check_board/check.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template  
+app = Flask(__name__) 
+@app.route("/")                           
+def hello_world():
+    
+    return " test server = working"
+@app.route("/<x>/<y>")
+def chek_board(x,y):
+    input1=int(x)
+    input2=int(y)
+    return render_template("index.html", input1=input1, input2=input2)
+if __name__ == "__main__":
+    app.run(debug = True)       
